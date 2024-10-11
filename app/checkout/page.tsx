b05521236b47cea627ade5308d411fc919923bf8
Sahil bhanushali <sahilbhanushali2004@gmail.com>
2024-10-12 01:38:46 +0530

    change in checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -2,20 +2,18 @@
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import { Spin } from 'antd';
-import { useState } from 'react';
 
 const Checkout = () => {
-  const [loader,setLoader] = useState(false)
+  // const [loader,setLoader] = useState(false)
   return (
-    <Spin spinning={loader}>
+    <div>
       <Navbar />
       <main className="px-4 py-8">
         <h1 className="text-2xl font-bold mb-6">Checkout</h1>
         {/* Add payment integration later here */}
       </main>
       <Footer />
-    </Spin>
+    </div>
   );
 };
 
